7554119b7f83a43b99977ae87cbf8cbce53143dd
Abhis080701 <abhis080701@gmail.com>
2025-02-24 18:40:35 +0530

    annoations

--- src/main/java/com/practoclone/model/Doctor.java
+++ src/main/java/com/practoclone/model/Doctor.java
@@ -1,17 +1,45 @@
 package com.practoclone.model;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "doctors")
 public class Doctor {
-	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private long id;
-    private String name;
-    private String email;
-    private String qualification;
-    private int experience;
-    private int age;
-    private long mobileNo;
-    private String address;
-    private boolean isAvailable;
-    
+	@Column(name = "name")
+	private String name;
+
+	@Column(name = "email")
+	private String email;
+
+	@Column(name = "qualification")
+	private String qualification;
+
+	@Column(name = "experience")
+	private int experience;
+
+	@Column(name = "age")
+	private int age;
+
+	@Column(name = "mobile_no")
+	private long mobileNo;
+
+	@Column(name = "address")
+	private String address;
+
+	@Column(name = "is_available")
+	private boolean isAvailable;
+
+	public Doctor() {
+	}
+
 	public Doctor(long id, String name, String email, String qualification, int experience, int age, long mobileNo,
 			String address, boolean isAvailable) {
 		super();
@@ -25,62 +53,77 @@ public class Doctor {
 		this.address = address;
 		this.isAvailable = isAvailable;
 	}
-	
+
 	public long getId() {
 		return id;
 	}
+
 	public void setId(long id) {
 		this.id = id;
 	}
+
 	public String getName() {
 		return name;
 	}
+
 	public void setName(String name) {
 		this.name = name;
 	}
+
 	public String getEmail() {
 		return email;
 	}
+
 	public void setEmail(String email) {
 		this.email = email;
 	}
+
 	public String getQualification() {
 		return qualification;
 	}
+
 	public void setQualification(String qualification) {
 		this.qualification = qualification;
 	}
+
 	public int getExperience() {
 		return experience;
 	}
+
 	public void setExperience(int experience) {
 		this.experience = experience;
 	}
+
 	public int getAge() {
 		return age;
 	}
+
 	public void setAge(int age) {
 		this.age = age;
 	}
+
 	public long getMobileNo() {
 		return mobileNo;
 	}
+
 	public void setMobileNo(long mobileNo) {
 		this.mobileNo = mobileNo;
 	}
+
 	public String getAddress() {
 		return address;
 	}
+
 	public void setAddress(String address) {
 		this.address = address;
 	}
+
 	public boolean isAvailable() {
 		return isAvailable;
 	}
+
 	public void setAvailable(boolean isAvailable) {
 		this.isAvailable = isAvailable;
 	}
-  
-    
 
 }
